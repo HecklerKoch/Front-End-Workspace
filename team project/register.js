850cc82baff80f9dcbb60408a77b0b5321b7c3df
HecklerKoch <wkd222xo@gmail.com>
2024-08-01 17:01:39 +0900

    과제

--- team project/register.js
+++ team project/register.js
@@ -5,9 +5,11 @@ userId.addEventListener("input", function () {
   const regExp = /^(?=.*[a-z])(?=.*\d).{0,20}$/;
   const idcheck = regExp.test(userId.value);
   if (idcheck) {
-    userIdSpan.style.color = "white";
+    userIdSpan.innerHTML = "";
   } else {
     userIdSpan.style.color = "red";
+    userIdSpan.innerHTML =
+      "* 아이디는 무조건 영소문자, 숫자 포함 20자 이하여야합니다.";
   }
 });
 
@@ -18,9 +20,11 @@ userPassword.addEventListener("input", function () {
   const regExp = /^(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/;
   const pwdcheck = regExp.test(userPassword.value);
   if (pwdcheck) {
-    userPasswordSpan.style.color = "white";
+    userPasswordSpan.innerHTML = "";
   } else {
     userPasswordSpan.style.color = "red";
+    userPasswordSpan.innerHTML =
+      "* 비밀번호는 영소문자, 숫자, 특수문자 포함 8자리 이상이여야합니다.";
   }
 });
 
@@ -31,8 +35,9 @@ userName.addEventListener("input", function () {
   const regExp = /^(?=.*[가-힣]).{2,10}$/;
   const namecheck = regExp.test(userName.value);
   if (namecheck) {
-    userNameSpan.style.color = "white";
+    userNameSpan.innerHTML = "";
   } else {
     userNameSpan.style.color = "red";
+    userNameSpan.innerHTML = "* 이름은 필수입니다! (2글자 이상)";
   }
 });
