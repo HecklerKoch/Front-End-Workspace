cf3e107c0c5c459afabd8c1a9b68dc16a68b0570
HecklerKoch <wkd222xo@gmail.com>
2024-08-13 18:11:18 +0900

    과제

--- team project/calander.js
+++ team project/calander.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var Calendar = FullCalendar.Calendar;
-  var Draggable = FullCalendar.Draggable;
 
-  var containerEl = document.getElementById("external-events");
   var calendarEl = document.getElementById("calendar");
-  var checkbox = document.getElementById("drop-remove");
 
   var calendar = new Calendar(calendarEl, {
     headerToolbar: {
@@ -14,15 +11,31 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     selectable: true,
     editable: true,
-    droppable: true, // this allows things to be dropped onto the calendar
-    drop: function (info) {
-      // is the "remove after drop" checkbox checked?
-      if (checkbox.checked) {
-        // if so, remove the element from the "Draggable Events" list
-        info.draggedEl.parentNode.removeChild(info.draggedEl);
+    dateClick: function (info) {
+      const month = calendarEl
+        .querySelector(".fc-toolbar-title")
+        .innerText.split(" ")[0];
+      var clickedDate = info.dateStr;
+      var selectedMonth = info.date.getMonth(); // 0부터 시작하는 월 인덱스
+      if (selectedMonth === 7 && month === "August") {
+        // 예: 7월 (8월의 경우 7로 설정)
+        showModal(clickedDate);
+      } else if (selectedMonth === 6 && month === "July") {
+        showModal(clickedDate);
       }
     },
   });
 
   calendar.render();
+
+  function showModal(date) {
+    var modal = document.getElementById("myModal");
+    var modalContent = document.getElementById("modalContent");
+    modalContent.innerHTML = "<p>Selected Date: " + date + "</p>";
+    modal.style.display = "block";
+  }
+
+  document.getElementById("X").addEventListener("click", function () {
+    document.getElementById("myModal").style.display = "none";
+  });
 });
